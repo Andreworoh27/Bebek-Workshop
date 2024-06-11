ef261385408d64fc0864f8325a2a8e26345b8d55
Andrew Oroh <andreworoh.ao@gmail.com>
2024-06-11 14:49:49 +0700

    Initial commit

---
+++ BebekWorkshop/BebekWorkshop/BebekWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BebekWorkshopApp.swift
+//  BebekWorkshop
+//
+//  Created by Andrew Oroh on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BebekWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BebekWorkshop/BebekWorkshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BebekWorkshop
+//
+//  Created by Andrew Oroh on 11/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
